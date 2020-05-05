2d55f2f6632910907563d513b6e8042c4c6002af
Martin <zexyqag@gmail.com>
2020-05-05 16:08:58 +0200

    Renamded test module to additiveStream, fisnished additiveStream functionality, and yote Player.py

---
+++ additiveStream.py
@@ -0,0 +1,28 @@
+import sounddevice as sd
+from itertools import zip_longest
+
+class AdditiveStream:
+    def __callback(self, outdata, frames, time, status):
+        outdata[:,0] = self.__getNext(frames)
+
+    def __init__(self):
+        self.buffer = [0]
+        self.stream = sd.OutputStream(channels = 1, callback = self.__callback)
+        self.start()
+
+    def start(self):
+        self.stream.start()
+
+    def stop(self):
+        self.stream.stop()
+
+    def __getNext(self, frames):
+        if frames > len(self.buffer):
+            print("end of buffer; extending")
+            self.buffer.extend([0] * (frames - len(self.buffer)))
+        out = self.buffer[:frames]
+        self.buffer = self.buffer[frames:]
+        return out
+
+    def play(self, sample):
+        self.buffer = [x + y for x,y in zip_longest(sample, self.buffer, fillvalue=0)]
