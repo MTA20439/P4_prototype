58a9de52e851da00fcb0c267c807910485ff7068
Martin <zexyqag@gmail.com>
2020-05-15 10:58:02 +0200

    Added 1 new fixed method 'karplusStrong' with properly adjustable length, Added 3 new methods to work with the fixed version of karplus strong, whitenoise to generate whitnoise based on the desired wavelength, fadeout to fade out the sting instead of it cutting off based on a start-stop parameter, generate guitar string which uses all of the aforementioned methods to generate a guitar string

--- Synthesizer.py
+++ Synthesizer.py
@@ -1,5 +1,4 @@
 import random
-from numba import njit
 import numpy as np
 
 
@@ -8,7 +7,7 @@ class StringSynthesizer:
         self.samplerate = samplerate
         self.amplitude = amplitude
         self.duration = duration
-        self.decay = decay
+        self.decay = float(np.clip(0, 1, float(decay)))
         self.octave = 1
 
     def makeSomeNoise(self):
@@ -26,3 +25,28 @@ class StringSynthesizer:
         for idx in range(delay, len(output)):
             output[idx] = decay * 0.5 * (output[idx - delay] + output[idx - delay - 1])
         return output
+
+    def whiteNoise(self, wavelength):
+        return [(random.uniform(-self.amplitude, self.amplitude)) for _ in range(int(wavelength))]
+
+    def karplusStrong(self, NoiseBurst, frequency):
+        delay = int(self.samplerate / (frequency * self.octave))
+        output = NoiseBurst
+        output.extend([0] * int(self.duration * self.samplerate))
+        for idx in range(delay, len(output)):
+            output[idx] = self.decay * 0.5 * (output[idx - delay] + output[idx - delay - 1])
+        return output
+
+    def fadeOut(self, sample, start = 0, end = 1):
+        if start >= end:
+            print("fadeOut 'end' must be larger than 'start'")
+            return sample
+        output = sample
+        startIdx = int(len(sample)*start)
+        endIdx = int(len(sample)*end)
+        for idx in range(startIdx, endIdx):
+            output[idx] = output[idx]*(1-((idx-startIdx)/endIdx))
+        return output
+
+    def generateGuitarString(self, frequency):
+        return self.fadeOut(self.karplusStrong(self.whiteNoise(self.samplerate / frequency), frequency))
